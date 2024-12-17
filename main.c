fc56d9646dd1b3a5354e2c1d681b61680e321bca
Joseph Logatto <Josephlogatto1424@gmail.com>
2024-12-16 22:28:15 -0500

    Fix for multiplication

--- main.c
+++ main.c
@@ -4,7 +4,17 @@ int add(int a, int b) {
     return a + b;
 }
 
+int subtract(int a, int b) {
+    return a - b;
+}
+
+int multiply(int a, int b) {
+    return a * b;
+}
+
 int main() {
-    printf("Calculator result: %d\n", add(5, 3));
+    printf("Addition result: %d\n", add(5, 3));
+    printf("Subtraction result: %d\n", subtract(5, 3));
+    printf("Multiplication result: %d\n", multiply(5, 3));
     return 0;
 }
